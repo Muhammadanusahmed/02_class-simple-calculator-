0124577aab083ee50c4f6e098e42341e02e91e2f
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-03-27 16:50:29 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer"
+
+console.log("     [WELLCOME]");
+
+const answer = await inquirer.prompt([
+    { message: "enter first num", type: "number", name: "firstNumber" },
+    { message: "enter second num", type: "number", name: "secondNumber" },
+    {
+      message: "select one oprator to perform operation",
+      type: "list",
+      name: "operator",
+      choices: ["addition", "subtraction", "multiplication", "division"]
+    },
+  ]);
+
+  // conditional statment
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber)
+    } else if (answer.operator === "subtraction") {
+        console.log(answer.firstNumber - answer.secondNumber)
+        } 
+    else if (answer.operator === "multiplication") {
+            console.log(answer.firstNumber * answer.secondNumber)
+            } 
+    else if (answer.operator === "division") {
+                console.log(answer.firstNumber / answer.secondNumber)
+                } 
+    else {
+        console.log("please select valid oprator")
+    }
+
+    console.log("  [PLEASE! USE AGAIN]");
+    
